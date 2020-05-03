1d64c8d22dd68cf389b5f60159d06c285ad55f79
Norris Nicholson <norrispnicholson@gmail.com>
2020-05-03 11:05:50 -0400

    Changed to the circuitpython module dependency

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     # Author details
     author="Norris Nicholson",
     author_email="",
-    install_requires=["Adafruit-Blinka", "supervisor",],
+    install_requires=["Adafruit-Blinka", "circuitpython",],
     # Choose your license
     license="MIT",
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
