019d1603568ed22d7cee895b9e5dfd6178a99cd5
Norris Nicholson <norrispnicholson@gmail.com>
2020-05-04 22:47:45 -0400

    Added support for colors

--- circuitpython_curses.py
+++ circuitpython_curses.py
@@ -64,7 +64,71 @@ class curses:
         """ Exception raised when a curses library function returns an error.
         """
 
-    cursor_pos = (0, 0)
+    COLOR_BLACK = 0
+    COLOR_BLUE = 4
+    COLOR_CYAN = 6
+    COLOR_GREEN = 2
+    COLOR_MAGENTA = 5
+    COLOR_RED = 1
+    COLOR_WHITE = 15
+    COLOR_YELLOW = 11
+    COLOR_NONE = -1
+
+    MODE_16 = 0
+    MODE_256 = 1
+
+    # TODO: Procedurally generate this instead to save memory if we don't need it?
+    _COLOR_256_16_FG = {
+        0: 30,
+        1: 31,
+        2: 32,
+        3: 33,
+        4: 34,
+        5: 35,
+        6: 36,
+        7: 37,
+        8: 90,
+        9: 91,
+        10: 92,
+        11: 93,
+        12: 94,
+        13: 95,
+        14: 96,
+        15: 97,
+    }
+
+    # TODO: Procedurally generate this instead to save memory if we don't need it?
+    _COLOR_256_16_BG = {
+        0: 40,
+        1: 41,
+        2: 42,
+        3: 43,
+        4: 44,
+        5: 45,
+        6: 46,
+        7: 47,
+        8: 100,
+        9: 101,
+        10: 102,
+        11: 103,
+        12: 104,
+        13: 105,
+        14: 106,
+        15: 107,
+    }
+
+    background = 0x3FE00
+    foreground = 0x1FF
+
+    # 0 = 16 colors
+    # 1 = 256 colors
+    color_mode = MODE_256
+
+    _color_pairs = {}
+
+    _cursor_pos = (0, 0)
+
+    _use_string_encoding = False
 
     def __init__(self):
         pass
@@ -73,6 +137,26 @@ class curses:
     def initscr():
         """ Initialize the library. Return a window object which represents the whole screen.
         """
+
+        # TODO: Find out which terminal we are on and what it supports:
+        # minicom: _use_sring_encoding = True
+        #          only 16 base colors: ESC[30;47m
+        # Screen:  _use_string_encoding = False
+        #          all colors
+
+        # Request the terminal askback
+        curses.write("\005")
+        time.sleep(0.1)
+
+        # See if we get a response. Some terminals will not return anything
+        if supervisor.runtime.serial_bytes_available:
+            # Read in one character
+            char = sys.stdin.read(1)
+
+            # If the character is M, then we might be on minicom
+            if char is "M":
+                curses._use_string_encoding = True
+                # color_mode = 0
 
         # Clear the entire screen
         curses.write("\033[2J")
@@ -88,7 +172,10 @@ class curses:
         # Clear the screen again
         curses.write("\033[2J")
 
-        curses.cursor_pos = (0, 0)
+        # Move the cursor to 0, 0
+        curses.write("\33[;H")
+
+        curses._cursor_pos = (0, 0)
 
         # Set up a serial capure unit
         counter = 0
@@ -132,6 +219,46 @@ class curses:
         """
 
     @staticmethod
+    def color_pair(color_number):
+        """ Return the attribute value for displaying text in the specified color. This attribute
+            value can be combined with A_STANDOUT, A_REVERSE, and the other A_* attributes.
+        """
+        try:
+            return curses._color_pairs[color_number]
+        except KeyError:
+            return 0
+
+    @staticmethod
+    def init_pair(pair_number, fg, bg):
+        """ Change the definition of a color-pair. It takes three arguments: the number of the
+            color-pair to be changed, the foreground color number, and the background color number.
+            The value of pair_number must be between 1 and COLOR_PAIRS - 1 (the 0 color pair is
+            wired to white on black and cannot be changed). The value of fg and bg arguments must
+            be between 0 and COLORS. If the color-pair was previously initialized, the screen is
+            refreshed and all occurrences of that color-pair are changed to the new definition.
+        """
+
+        # Encode negative values as 256 for the background and foreground
+        if fg < 0:
+            fg = 256
+
+        if bg < 0:
+            bg = 256
+
+        # 01234567 012345678 012345678
+        # other    bckgnd    frgnd
+        curses._color_pairs[pair_number] = bg << 9 | fg
+
+    @staticmethod
+    def start_color():
+        """ start_color() initializes eight basic colors (black, red, green, yellow, blue, magenta,
+            cyan, and white), and two global variables in the curses module, COLORS and COLOR_PAIRS,
+            containing the maximum number of colors and color-pairs the terminal can support.
+
+            Stub - not used for circuitpython implementation
+        """
+
+    @staticmethod
     def newwin(nlines, ncols, begin_y=0, begin_x=0):
         """ Return a new window, whose left-upper corner is at (begin_y, begin_x),
             and whose height/width is nlines/ncols.
@@ -147,14 +274,14 @@ class curses:
         print(text, end="")
 
     @staticmethod
-    def write_pos(y, x, string):
+    def write_pos(y, x, string, prefix, postfix):
         """ Prints to the console without a newline at the specified location
         """
 
         # Calculate the current offset between where the cursor is, and where
         # we want it to be
-        y_offset = y - curses.cursor_pos[0]
-        x_offset = x - curses.cursor_pos[1]
+        y_offset = y - curses._cursor_pos[0]
+        x_offset = x - curses._cursor_pos[1]
 
         # Add the approperate strings to the buffer as needed to move
         # the cursor to where it needs to be
@@ -167,23 +294,31 @@ class curses:
         elif x_offset < 0:
             curses.write(escape.cub(-x_offset))
 
-        # Write the buffer string plus the desired string to the
-        # console
+        # If there is a prefix to write, do so first
+        if prefix:
+            curses.write(prefix)
+
+        # Then write the string
         curses.write(string)
+
+        # Then write any postfix
+        if postfix:
+            curses.write(postfix)
 
         # Adjust the cursor position based on the length of the string
         # we just wrote. We encode the string so that we catch any unicode
         # characters in the string and adjust as needed. This is only
         # needed to measure the visual length of the string on the screen.
-        # Calling .encode() on a string with escape sequences will remove
-        # the sequences
-        curses.cursor_pos = (y, x + len(string.encode()))
+        #
+        # We only measure the proper string for moving the cursor. All escapes should be
+        # placed in the prefix and postfix
+        if curses._use_string_encoding:
+            curses._cursor_pos = (y, x + len(string.encode()))
+        else:
+            curses._cursor_pos = (y, x + len(string))
 
         # Add one to the y offset for the cup method
         # curses.write("\33[" + str(y + 1) + ";" + str(x) + "H" + string)
-
-        # curses.write("\033[0;0H\033[" + str(y) +
-        #              "B\033[" + str(x) + "C" + string)
 
     @staticmethod
     def beep():
@@ -218,6 +353,42 @@ class curses:
         while supervisor.runtime.serial_bytes_available:
             sys.stdin.read(1)
 
+    @staticmethod
+    def _get_fg(attr):
+        """ Decodes the attr var to get the foreground color
+        """
+        _cl = attr & curses.foreground
+        return None if _cl is 256 else _cl
+
+    @staticmethod
+    def _get_bg(attr):
+        """ Decodes the attr var to get the background color
+        """
+        _cl = (attr & curses.background) >> 9
+        return None if _cl is 256 else _cl
+
+    @staticmethod
+    # TODO: May not need this method anymore
+    def clean_ansi(string):
+        """ Cleans a color ansi code out of a string
+        """
+        # Get the location of the first escape sequence. If non exists, this will return a -1 and
+        # the loop will be skipped
+        _l = string.find("\33[")
+        # While there is another escape sequence to remove, loop
+        while _l is not -1:
+            # Cut the sequence out of the string. Starting at the escape sequence, look for
+            # the letter "m", which denotes the end of the sequence. Set the string to be that
+            # portion of the string which is before and after the escape.
+            string = string[:_l] + string[string.find("m", _l) + 1 :]
+
+            # Find the next escape sequence, if there is one. If non exists, this will return a -1
+            # and the loop will end
+            _l = string.find("\33[")
+
+        # Return the modified string
+        return string
+
 
 # For compatability with the curses codebase, ignore this pylint error
 # pylint: disable=invalid-name
@@ -230,14 +401,148 @@ class window:
         self.x_pos = x_pos
         self.y_pos = y_pos
 
-    def addstr(self, y, x, string):
+    def addnstr(self, y, x, n, string, attr=None):
+        """ Paint at most n characters of the character string str at (y, x) with attributes attr,
+            overwriting anything previously on the display.
+        """
+        return self.addstr(y, x, string[0:n], attr)
+
+    def addstr(self, y, x, string, attr=None):
         """ Paint the character string str at (y, x) with attributes attr, overwriting anything
             previously on the display.
         """
-        if not self._in_bounds(y, x, string):
-            # raise curses.error(f"Out of Bounds: ({y},{x}) {self.window_size}")
+
+        # TODO: Add support for A_ attributes (like blink, underline, etc)
+
+        # Set prefix and postfix to None here incase they are not set to something else later
+        prefix = None
+        postfix = None
+
+        # See if there is an attribute
+        if attr:
+            # pylint: disable=protected-access
+            fg = curses._get_fg(attr)
+            bg = curses._get_bg(attr)
+
+            # 16 color mode
+            if curses.color_mode is curses.MODE_16:
+
+                # TODO: Add support for intensity (currently set at 1)
+                # We are using 16 colors
+                if fg or bg:
+                    postfix = "\33[m"
+                    # Try to translate the color from 256 to 16. If there is a key error, then there
+                    # is not a translation between the color modes. In that case, we write nothing
+                    # to prefix
+                    try:
+                        if fg:
+                            fg = curses._COLOR_256_16_FG[fg]
+                    except KeyError:
+                        fg = curses._COLOR_256_16_FG[curses.COLOR_BLACK]
+                    try:
+                        if bg:
+                            bg = curses._COLOR_256_16_BG[bg]
+                    except KeyError:
+                        bg = curses._COLOR_256_16_BG[curses.COLOR_BLACK]
+
+                    if fg and bg:
+                        prefix = "\33[1;" + str(fg) + ";1;" + str(bg) + "m"
+                    else:
+                        # If fg and bg arent set, then only one of them is. Use which ever setting
+                        # only
+                        prefix = "\33[1;" + str(fg or bg) + "m"
+
+            # 256 color mode
+            else:
+                if fg or bg:
+                    postfix = "\33[m"
+                    if fg and bg:
+                        prefix = "\33[38;5;" + str(fg) + ";48;5;" + str(bg) + "m"
+                    elif fg:
+                        prefix = "\33[38;5;" + str(fg) + "m"
+                    else:  # bg
+                        prefix = "\33[48;5;" + str(bg) + "m"
+
+        # TODO: Wrapping does not seem to work with special characters (eg: ▆)
+
+        # Split the input string by newline characters and grab each split string as an individual.
+        # If there are no newline characters in the string, the for loop will run only once
+        lst = string.split("\n")
+        lst_len = len(lst) - 1  # This line minus one to reduce work in the loop
+        for idx, line in enumerate(lst):
+
+            # While this split string is longer than the window, starting at the given x position,
+            # cut it into smaller strings and simulate a newline
+            while x + len(line.encode()) > self.window_size[1]:
+
+                # Cut the string so that it will fit in this line
+                self._addstr(y, x, line[0 : self.window_size[1] - x], prefix=prefix)
+
+                # Set the prefix to an empty string because we have already written it to the
+                # screen
+                prefix = ""
+
+                # Move to the next line, and set the cursor to the start of the window
+                y = y + 1
+                x = 0
+
+                # Use the remainder of the cut string above for next time loop
+                line = line[self.window_size[1] - x :]
+
+            # After the string has been cut until it can fit in a single line, print the string. If
+            # the string was never bigger than a single line, then only this _addstr will run. If
+            # we are not at the end of our for loop, then we have arrived at a newline character.
+            # In this case, print the line plus padded spaces to the end of the line. This is to
+            # replicate curses
+            if idx < lst_len:
+                self._addstr(
+                    y,
+                    x,
+                    line + " " * (self.window_size[1] - len(line.encode())),
+                    prefix=prefix,
+                )
+
+                # Set the prefix to an empty string because we have already written it to the
+                # screen
+                prefix = ""
+                # Now we have reached the end of the first split string. If there are more strings
+                # split based on newline characters, then we want to prep for them next loop. Go
+                # ahead and move to the next line, and set the x position to 0
+                y = y + 1
+                x = 0
+            else:
+                # Since this will always be run at least once, at the very end of the string, we
+                # add the postfix here
+                self._addstr(y, x, line, prefix=prefix, postfix=postfix)
+
+                # Don't need to worry about clearing prefix or postfix here because this is the
+                # end of the loop
+
+    def _addstr(self, y, x, string, prefix=None, postfix=None):
+        """ Internal function for writing to the screen. Checks to ensure that the given string
+            will not exceed the boundries of the window.
+
+            Also checks to ensure that the last character in the window is not written to. This 
+            seems to be a quirk with curses that needs to be emulated.
+        """
+
+        str_len = len(string.encode())
+
+        # Ensure that we have not written up to the last character in the window. This is
+        # a quirk with curses that needs to be replicated
+        if (
+            # Ensure that y is not greater than the window size
+            y > self.window_size[0]
+            # Ensure that y is not negative
+            or y < 0
+            # Ensure that x plus whatever we are writing to the screen is less than or equal to
+            # the width of the window
+            or x + str_len > self.window_size[1]
+            # Ensure that x is not negative
+            or x < 0
+        ):
             raise curses.error(
-                "Out of Bounds: ("
+                "A Out of Bounds: ("
                 + str(y)
                 + ","
                 + str(x)
@@ -245,39 +550,44 @@ class window:
                 + str(self.window_size)
             )
 
-        curses.write_pos(y + self.y_pos, x + self.x_pos, string)
+        # Make sure that the last character in the window will not be written to
+        if y >= self.window_size[0] and x + str_len >= self.window_size[1]:
+            raise curses.error("Can't write to the last cell in a window")
+
+        # Write the string to the screen
+        self._write_pos(y, x, string, prefix=prefix, postfix=postfix)
+
+    def _write_pos(self, y, x, string, prefix=None, postfix=None):
+        """ Internal function for executing the curses.write_pos command, with the correct
+            offset based on this window's position.
+
+            This function does no verification of the location of the draw. Therefore, it is
+            possible with this function to draw outside the bounds of the window
+        """
+        curses.write_pos(
+            y + self.y_pos, x + self.x_pos, string, prefix=prefix, postfix=postfix
+        )
 
     def box(self):
         """ Draw a box around the edges of the window.
         """
-        self.addstr(0, 0, "┌")
+        self._write_pos(0, 0, "┌")  # ┌╭
         for x in range(1, self.window_size[1]):
-            self.addstr(0, x, "─")
+            self._write_pos(0, x, "─")
 
-        self.addstr(0, self.window_size[1] - 1, "┐")
+        self._write_pos(0, self.window_size[1] - 1, "┐")  # ┐╮
         for y in range(1, self.window_size[0]):
-            self.addstr(y, self.window_size[1] - 1, "│")
+            self._write_pos(y, self.window_size[1] - 1, "│")
 
-        self.addstr(self.window_size[0], self.window_size[1] - 1, "┘")
+        self._write_pos(self.window_size[0], self.window_size[1] - 1, "┘")  # ┘╯
 
         for x in range(self.window_size[1] - 2, 0, -1):
-            self.addstr(self.window_size[0], x, "─")
+            self._write_pos(self.window_size[0], x, "─")
 
-        self.addstr(self.window_size[0], 0, "└")
+        self._write_pos(self.window_size[0], 0, "└")  # └╰
 
         for y in range(self.window_size[0] - 1, 0, -1):
-            self.addstr(y, 0, "│")
-
-    def _in_bounds(self, y, x, string):
-        if (
-            y > self.window_size[0]
-            or y < 0
-            or x + len(string) > self.window_size[1]
-            or x < 0
-        ):
-            return False
-
-        return True
+            self._write_pos(y, 0, "│")
 
     def refresh(self):
         """ Update the display immediately. Stub - not used for circuit python implementation
@@ -287,6 +597,11 @@ class window:
         """ Return a tuple (y, x) of the height and width of the window.
         """
         return (self.window_size[0] + 1, self.window_size[1])
+
+    def getbegyx(self):
+        """ Return a tuple (y, x) of co-ordinates of upper-left corner.
+        """
+        return (self.y_pos, self.x_pos - 1)
 
     # For compatability with the curses codebase, ignore this pylint error
     # pylint: disable=no-self-use
--- examples/curses_simpletest.py
+++ examples/curses_simpletest.py
@@ -8,41 +8,51 @@ import time
 import supervisor
 from circuitpython_curses import curses
 
+# print("\33[0m", end="")
+
+
+# st = "\33[38;5;55;48;5;196mthis is a test!\33[0m"
+
+# print(curses.clean_ansi(st).encode())
+
+# #
+
+# # print("\33[38;5;55;48;5;196mthis is a test!")
+# # print("\33[38;2;0;128;128mthis is a test!")
+
+
+# # print("\33[30;47mthis is a test!")
+# # print("\33[38;5;27mthis is a test!", end="")
+
+# # print("\33[38;5;27mThis is a test!")
+
+
+# sys.exit()
+
 try:
     curses.curs_set(1)
     STDSCR = curses.initscr()
 
-    # stdscr.addstr(0, 0, "#")
+    curses.init_pair(1, 207, -1)
+    curses.init_pair(2, -1, 123)
+    curses.init_pair(3, 207, 123)
+    curses.init_pair(4, -1, -1)
+    curses.init_pair(5, curses.COLOR_RED, -1)
 
-    # sys.exit()
-    # curses.noecho()
+    # for y in range(0, STDSCR.getmaxyx()[0]):
+    #     for x in range(0, STDSCR.getmaxyx()[1] - 1):
+    #         STDSCR.addstr(y, x, "#")
+    #         STDSCR.refresh()
 
-    # stdscr.addstr(9, 10, "@")
-    # stdscr.addstr(10, 9, "@")
+    # for y in range(0, STDSCR.getmaxyx()[0]):
+    #     for x in range(0, STDSCR.getmaxyx()[1] - 1):
+    #         STDSCR.addstr(y, x, ".")
+    #         STDSCR.refresh()
 
-    # stdscr.addstr(20, 9, "@")
-    # stdscr.addstr(21, 10, "@")
-
-    # stdscr.addstr(21, 30, "@")
-    # stdscr.addstr(20, 31, "@")
-
-    # stdscr.addstr(10, 31, "@")
-    # stdscr.addstr(9, 30, "@")
-
-    for y in range(0, STDSCR.getmaxyx()[0]):
-        for x in range(0, STDSCR.getmaxyx()[1]):
-            STDSCR.addstr(y, x, "#")
-            STDSCR.refresh()
-
-    for y in range(0, STDSCR.getmaxyx()[0]):
-        for x in range(0, STDSCR.getmaxyx()[1]):
-            STDSCR.addstr(y, x, ".")
-            STDSCR.refresh()
-
-    for y in range(0, STDSCR.getmaxyx()[0]):
-        for x in range(0, STDSCR.getmaxyx()[1]):
-            STDSCR.addstr(y, x, " ")
-            STDSCR.refresh()
+    # for y in range(0, STDSCR.getmaxyx()[0]):
+    #     for x in range(0, STDSCR.getmaxyx()[1] - 1):
+    #         STDSCR.addstr(y, x, " ")
+    #         STDSCR.refresh()
 
     STDSCR.addstr(9, 10, "A")
     STDSCR.addstr(10, 9, "B")
@@ -56,16 +66,17 @@ try:
     STDSCR.addstr(10, 30, "G")  # 10, 31
     STDSCR.addstr(9, 29, "H")  # 9,  30
 
-    STDSCR.addstr(9, 40, "Win: 10x20 @ 10,10")
-    STDSCR.addstr(10, 42, "#:  Wx h    x, y")
-    STDSCR.addstr(11, 42, "A:          9,10")
-    STDSCR.addstr(12, 42, "B:         10, 9")
-    STDSCR.addstr(13, 42, "C:         19, 9")
-    STDSCR.addstr(14, 42, "D:         20,10")
-    STDSCR.addstr(15, 42, "E:         20,29")
-    STDSCR.addstr(16, 42, "F:         19,30")
-    STDSCR.addstr(17, 42, "G:         10,30")
-    STDSCR.addstr(18, 42, "H:          9,29")
+    STDSCR.addstr(9, 42, "#:   Wx  h     x, y")
+    STDSCR.addstr(10, 40, "Win: (10, 20)  (10, 10)")
+
+    STDSCR.addstr(12, 42, "A:           ( 9, 10)", curses.color_pair(1))
+    STDSCR.addstr(13, 42, "B:           (10,  9)", curses.color_pair(2))
+    STDSCR.addstr(14, 42, "C:           (19,  9)", curses.color_pair(3))
+    STDSCR.addstr(15, 42, "D:           (20, 10)")
+    STDSCR.addstr(16, 42, "E:           (20, 29)")
+    STDSCR.addstr(17, 42, "F:           (19, 30)")
+    STDSCR.addstr(18, 42, "G:           (10, 30)")
+    STDSCR.addstr(19, 42, "H:           ( 9, 29)")
 
     STDSCR.addstr(2, 3, f"Window Size: {STDSCR.getmaxyx()}")
 
@@ -85,19 +96,9 @@ try:
     WIN.addstr(5, 6, "3: 8,18")
     WIN.addstr(6, 6, "4: 8, 1")
 
+    STDSCR.addstr(11, 40, f"Pos: {WIN.getmaxyx()}  {WIN.getbegyx()} ")
+
     WIN.refresh()
-
-    # for i in range(126, 1000):
-    #     STDSCR.addstr(0, 0, chr(i))
-    #     # curses.cursor_pos = (
-    #     #     curses.cursor_pos[0], curses.cursor_pos[1]+len(chr(i)))
-    #     # STDSCR.addstr(1, 0, str(i))
-    #     # STDSCR.addstr(2, 0, str(len(chr(i))))
-    #     time.sleep(0.01)
-    # curses.cursor_pos = (
-    # curses.cursor_pos[0], curses.cursor_pos[1]+2)
-
-    # STDSCR.box()
 
     WIN2 = curses.newwin(5, 5, 20, 40)
     WIN2.box()
@@ -107,18 +108,48 @@ try:
     WIN2.addstr(2, 1, "ABC")
     WIN2.addstr(3, 1, "ABC")
 
+    WIN3 = curses.newwin(6, 20, 25, 10)
+    WIN3.box()
+
+    # WIN3.addstr(
+    #     0,
+    #     0,
+    #     "0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789a",
+    # )
+
+    WIN3.addstr(
+        0,
+        0,
+        "0123456789abcdefghijklmnopqrstuvwxyz012345678uvwxyz\nThis is a test! and so is \nthis and bla",
+        curses.color_pair(5),
+    )
+
+    # WIN3.addstr(
+    #     0, 0, "▆" * 1,
+    # )
+
+    # WIN3.addstr(5, 19, "#")
+
+    WIN3.refresh()
+
+    # STDSCR.addstr(4, 3, f"Window Pos: {WIN.getbegyx()}")
+
     for y in range(0, STDSCR.getmaxyx()[0]):
         for x in range(81, STDSCR.getmaxyx()[1] - 2):
-            STDSCR.addstr(y, x, "#")
+            STDSCR.addstr(y, x, "#", (x * y) % 15)
             STDSCR.refresh()
 
-    for y in range(0, STDSCR.getmaxyx()[0], 1):
-        STDSCR.addstr(y, 3, str(y))
-        STDSCR.refresh()
+    # for y in range(0, STDSCR.getmaxyx()[0], 1):
+    #     STDSCR.addstr(y, 3, str(y))
+    #     STDSCR.refresh()
 
-    for x in range(0, STDSCR.getmaxyx()[1], 1):
-        STDSCR.addstr(3, x, str(x % 10))
-        STDSCR.refresh()
+    # for x in range(0, STDSCR.getmaxyx()[1], 1):
+    #     STDSCR.addstr(3, x, str(x % 10))
+    #     STDSCR.refresh()
+
+    # curses.init_pair(1, curses.red, 120)
+    # print(curses.color_pair(1)
+
 
 finally:
     curses.curs_set(0)
